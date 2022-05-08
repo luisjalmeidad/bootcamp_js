9b46316b0b194ff851dad0993e41926e1b9726be
Luis Almeida <luisjalmeidad@gmail.com>
2022-05-08 13:18:32 -0300

    bug fix bkpersons

--- ej_2-11_2-14/phonebook/src/App.js
+++ ej_2-11_2-14/phonebook/src/App.js
@@ -1,23 +1,28 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from "react";
 
-import './App.css';
-import Filter from './components/Filter';
-import PersonForm from './components/PersonForm';
-import Persons from './components/Persons';
+import "./App.css";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
 
 function App() {
   const [persons, setPersons] = useState([]);
   const [backupPersons, setBackupPersons] = useState([]);
   const [notFound, setNotFound] = useState(false);
 
-  const newName = useRef('');
-  const newNumber = useRef('');
-  const filter = useRef('');
+  const newName = useRef("");
+  const newNumber = useRef("");
+  const filter = useRef("");
 
   const addRecord = (event) => {
     event.preventDefault();
-    const newPerson = { name: newName.current.value, number: newNumber.current.value };
-    const personExists = persons.some((person) => person.name === newPerson.name);
+    const newPerson = {
+      name: newName.current.value,
+      number: newNumber.current.value,
+    };
+    const personExists = persons.some(
+      (person) => person.name === newPerson.name
+    );
     if (!personExists) {
       setPersons((prevPersons) => [...prevPersons, newPerson]);
       setBackupPersons((prevPersons) => [...prevPersons, newPerson]);
@@ -27,12 +32,13 @@ function App() {
   };
 
   const searchFilter = () => {
-    console.log(filter.current.value.length);
     if (filter.current.value.length < 1) {
       setPersons(backupPersons);
     } else {
-      const search = filter.current.value.toLocaleLowerCase();
-      const filteredPersons = persons.filter((person) => person.name.toLocaleLowerCase().includes(search));
+      const search = filter.current.value.toLowerCase();
+      const filteredPersons = backupPersons.filter((person) =>
+        person.name.toLowerCase().includes(search)
+      );
       if (filteredPersons.length !== 0) {
         setNotFound(false);
         setPersons(filteredPersons);
@@ -43,30 +49,44 @@ function App() {
   };
 
   useEffect(() => {
-    if (backupPersons.length === 0) {
-      setBackupPersons(persons);
-    }
-  }, [backupPersons]);
+    const requestData = async () => {
+      const requestJson = await fetch(
+        "http://localhost:3001/persons"
+      );
+      const data = await requestJson.json();
+      setBackupPersons(data);
+      setPersons(data);
+    };
+    requestData();
+  }, []);
 
-  useEffect(()=>{
-      const request = async () => {
-        const requestJson = await fetch('http://localhost:3001/persons');
-        console.log({requestJson});
-        const data = await requestJson.json();
-        console.log({data});
-        setBackupPersons(data); 
-        setPersons(data);
-      } 
-      request();
-      
-  },[])
+  /* useEffect(() => {
+    const postPersons = async () => {
+      const request = await fetch("http://localhost:3001/persons", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newPerson),
+      });
+      const requestJson = await request.json();
+    };
+  }); */
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter filter={filter} searchFilter={searchFilter} notFound={notFound} />
+      <Filter
+        filter={filter}
+        searchFilter={searchFilter}
+        notFound={notFound}
+      />
       <h2>Add a new </h2>
-      <PersonForm addRecord={addRecord} newName={newName} newNumber={newNumber} />
+      <PersonForm
+        addRecord={addRecord}
+        newName={newName}
+        newNumber={newNumber}
+      />
       <h2>Numbers</h2>
       <Persons persons={persons} />
     </div>
